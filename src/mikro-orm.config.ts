75b3d32dd6484b4692d4c26860b9e0c0e7f88d5b
Harry Prior <harry.prior@gfk.com>
2021-04-27 15:16:14 +0100

    Fix path import

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,7 +1,7 @@
 import {Post} from "./entities/Post";
 import {__prod__} from "./constants";
 import {MikroORM} from "@mikro-orm/core";
-import path from "node:path";
+const path = require('path');
 import {dbPass} from "./secrets";
 
 export default {
@@ -22,5 +22,6 @@ Separating this config file enables our configuration to be available through th
 We set this up in package.json
 
 Entities: objects which correspond to db entities such as tables
-Migrations:
+Migrations: when running the command npx mikro-orm migrations:create, this will generate sql code for generating
+            tables with the pg db, based on the Entities provided
  */
