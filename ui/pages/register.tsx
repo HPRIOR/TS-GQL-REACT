9a63b8f3676343c60b7259a04cbe2c5a0d2fbe15
Harry Prior <harry.prior@gfk.com>
2021-04-30 12:38:52 +0100

    Add gql query

--- ui/pages/register.tsx
+++ ui/pages/register.tsx
@@ -1,32 +1,53 @@
 import React from 'react';
-import {Button, FormControl, FormErrorMessage, FormLabel, Input} from "@chakra-ui/react";
+import {Box, Button, FormControl, FormErrorMessage, FormLabel, Input} from "@chakra-ui/react";
 import {Form, Formik} from "formik";
-import {Wrapper} from "./components/Wrapper";
+import {Wrapper} from "../components/Wrapper";
+import {InputField} from "../components/InputField";
+import {useMutation} from "urql";
 
-interface registerProps {
+interface RegisterProps {
 
 }
 
+const REGISTER_MUT =
+    `
+        mutation Register($username: String!, $password: String!) {
+            register(options: { username: $username, password: $password }) {
+                errors {
+                    message
+                }
+                user {
+                    id
+                    username
+                    updatedAt
+                }
+            }
+        }
+    `
 
-export const Register: React.FC<registerProps> = ({}) => {
+
+export const Register: React.FC<RegisterProps> = ({}) => {
+    const [, register] = useMutation(REGISTER_MUT);
     return (
         <Wrapper variant={"small"}>
             <Formik
                 initialValues={{username: "", password: ""}}
-                onSubmit={(values) => console.log(values)}
+                onSubmit={async (values) => {
+                    const response = await register(values)
+                    return response;
+                }}
             >
-                {({values, handleChange}) => (
+                {({isSubmitting}) => (
                     <Form>
-                        <FormControl>
-                            <FormLabel htmlFor="username">Username</FormLabel>
-                            <Input
-                                value={values.username}
-                                onChange={handleChange}
-                                id="username"
-                                placeholder="username"
-                            />
-                            {/*<FormErrorMessage>{form.errors.name}</FormErrorMessage>*/}
-                        </FormControl>
+                        <Box mt={4}>
+                            <InputField name="username" placeholder="username" label="Username"/>
+                        </Box>
+                        <Box mt={4}>
+                            <InputField name="password" placeholder="******" label="Password" type="password"/>
+                        </Box>
+                        <Box mt={4}>
+                            <Button colorScheme='teal' isLoading={isSubmitting} type='submit'>Register</Button>
+                        </Box>
                     </Form>
                 )}
             </Formik>
