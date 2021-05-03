e6cd8aa10212981517769cb721ff67f0a18929cd
Harry Prior <harry.prior@gfk.com>
2021-05-03 14:41:07 +0100

    Add validation for registration input

---
+++ api/src/utils/validateRegister.ts
@@ -0,0 +1,30 @@
+import {UsernamePasswordInput} from "../resolvers/UsernamePasswordInput";
+
+const validateRegister = (options: UsernamePasswordInput) => {
+    const errors = []
+    if (options.username.length <= 2) {
+        errors.push(
+            {
+                message: "username length must be greater than 2",
+            });
+    }
+
+    if (options.username.includes("@")) {
+        errors.push(
+            {
+                message: "Username cannot include an @",
+            }
+        );
+    }
+
+    if (options.password.length <= 2) {
+         errors.push(
+            {
+                message: "password length must be greater than 2",
+            }
+         );
+    }
+    return errors.length > 0 ? errors : null;
+};
+
+export default validateRegister;
